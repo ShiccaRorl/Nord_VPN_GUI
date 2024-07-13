6539389b9490e02508726a61f0efd555ad4bd368
ShiccaRorl <37141533+ShiccaRorl@users.noreply.github.com>
2024-07-13 20:19:50 +0900

    Update Nord_VPN.sh

--- Nord_VPN.sh
+++ Nord_VPN.sh
@@ -3,12 +3,17 @@
 # NordVPN Control Script
 
 function show_help() {
-    echo "Usage: $0 {connect|disconnect|status|set|help}"
-    echo "  connect [country]   Connect to VPN. Optionally specify country."
-    echo "  disconnect          Disconnect from VPN."
-    echo "  status              Show VPN connection status."
+    echo "Usage: $0 {connect|disconnect|status|set|get|list|current|reconnect|logout|help}"
+    echo "  connect [country]     Connect to VPN. Optionally specify country."
+    echo "  disconnect            Disconnect from VPN."
+    echo "  status                Show VPN connection status."
     echo "  set <setting> <value> Set a NordVPN configuration (e.g., autoconnect, killswitch)."
-    echo "  help                Show this help message."
+    echo "  get <setting>         Get the current value of a NordVPN configuration."
+    echo "  list                  List available NordVPN servers."
+    echo "  current               Show the current connected server information."
+    echo "  reconnect             Reconnect to the last connected server."
+    echo "  logout                Log out from NordVPN."
+    echo "  help                  Show this help message."
 }
 
 function connect_vpn() {
@@ -36,6 +41,31 @@ function set_option() {
     fi
 }
 
+function get_option() {
+    if [ -n "$1" ]; then
+        nordvpn settings | grep "$1"
+    else
+        echo "Error: Missing setting."
+        show_help
+    fi
+}
+
+function list_servers() {
+    nordvpn countries
+}
+
+function current_server() {
+    nordvpn status | grep 'Current server'
+}
+
+function reconnect_vpn() {
+    nordvpn reconnect
+}
+
+function logout_vpn() {
+    nordvpn logout
+}
+
 case "$1" in
     connect)
         connect_vpn "$2"
@@ -48,6 +78,21 @@ case "$1" in
         ;;
     set)
         set_option "$2" "$3"
+        ;;
+    get)
+        get_option "$2"
+        ;;
+    list)
+        list_servers
+        ;;
+    current)
+        current_server
+        ;;
+    reconnect)
+        reconnect_vpn
+        ;;
+    logout)
+        logout_vpn
         ;;
     help)
         show_help
